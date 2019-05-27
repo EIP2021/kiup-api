13b905542b920c843f5c6ce7bce297a9deb1d06b
jdereynal <jacques.de-reynal@epitech.eu>
2019-05-27 11:31:35 +0200

    added password.js to router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const auth = require('./auth');
+const password = require('./password');
 
 const router = express.Router();
 
 router.use(auth);
+router.use(password);
 
 module.exports = router;
