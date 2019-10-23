7b523dbc820727c4f1fbc4264bfa8979cdb6c3d2
Jacques de Reynal <jacques.de-reynal@epitech.eu>
2019-10-23 19:40:54 +0200

    [ADD] UserInfo model & changed register route (#3)

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,8 @@
 const User = require('./User');
+const UserInfo = require('./UserInfo');
+
 
 module.exports = {
   User,
+  UserInfo
 };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,15 +9,15 @@ const config = require('../../config.json');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, info } = req.body;
   const emailReg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const passwordReg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
   if (!req.body || !email || email.trim() === '' || email.match(emailReg) === null) {
-    return error(400, 'Invalid email', res);
+    return error(400, 'Adresse email invalide', res);
   }
   if (password.trim() !== '' && password.match(passwordReg) === null) {
-    return (error(400, 'Le mot de passe doit contenir au moins 8 caracteres et comporter des lettres majuscules et minuscules ainsi que des chiffres', res));
+    return (error(400, 'Le mot de passe doit contenir au moins 8 caracteres et comporter des majuscules, minuscules et chiffres', res));
   }
   try {
     const hash = await bcrypt.hash(password, config.SALT_ROUNDS);
@@ -27,23 +27,27 @@ router.post('/register', async (req, res) => {
       token: null,
       tokenExpires: null,
     });
-    if (!user) {
-      throw new Error('Couldn\'t create user');
+    const userInfo = await models.UserInfo.create({
+      id: user.id,
+      ...info,
+    });
+    if (!user || !userInfo) {
+      throw new Error('Une erreur est survenue lors de la creation du compte');
     }
     return res.json({
       error: false,
-      message: 'Successfully registered',
+      message: 'Enregistré avec succès',
     });
   } catch (err) {
     console.error(err);
-    return error(500, 'Internal server error', res);
+    return error(500, 'Erreur interne', res);
   }
 });
 
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return error(400, 'Invalid request', res);
+    return error(400, 'Requete invalide', res);
   }
   const user = await models.User.findOne({
     where: {
@@ -51,11 +55,11 @@ router.post('/login', async (req, res) => {
     },
   });
   if (!user) {
-    return error(403, 'Invalid email or password', res);
+    return error(403, 'Email ou mot de passe invalide', res);
   }
   const cmp = await bcrypt.compare(password, user.password);
   if (!cmp) {
-    return error(403, 'Invalid email or password', res);
+    return error(403, 'Email ou mot de passe invalide', res);
   }
   res.status(200).json({
     error: false,
