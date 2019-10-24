12831801e02a4a838f6743f18483745d431f5bc2
Jacques de Reynal <jacques.de-reynal@epitech.eu>
2019-10-24 16:19:15 +0200

    [ADD] Profile get and post routes. (#4)

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const auth = require('./auth');
 const password = require('./password');
+const profile = require('./profile');
 
 const router = express.Router();
 
 router.use(auth);
 router.use(password);
+router.use(profile);
 
 module.exports = router;
