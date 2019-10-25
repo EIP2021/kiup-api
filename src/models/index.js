7465d59092df7efe661111f6197ca604377cad12
jdereynal <jacques.de-reynal@epitech.eu>
2019-10-25 14:38:17 +0200

    [ADD] /stats GET and POST

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,10 @@
 const User = require('./User');
 const UserInfo = require('./UserInfo');
+const Consumption = require('./Consumption');
 
 
 module.exports = {
   User,
-  UserInfo
+  UserInfo,
+  Consumption,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const auth = require('./auth');
 const password = require('./password');
 const profile = require('./profile');
+const stats = require('./stats');
 
 const router = express.Router();
 
 router.use(auth);
 router.use(password);
 router.use(profile);
+router.use(stats);
 
 module.exports = router;
